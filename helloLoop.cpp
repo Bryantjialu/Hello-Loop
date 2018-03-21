7f3c5f1329453c36994d83e06f78d02726458556
bryantjialu <847124185@qq.com>
2018-03-21 10:31:10 +0800

    add main file

---
+++ helloLoop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+
+int main(){
+    
+    while(true){
+        cout << "111" << endl;
+    }
+
+
+    system("pause");
+    return 0;
+}
