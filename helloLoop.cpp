68f775382cd5c728de06e158fbaf1c3d1dc8929e
root <847124185@qq.com>
2018-03-21 14:27:57 +0800

    [c]

--- helloLoop.cpp
+++ helloLoop.cpp
@@ -1,31 +1,38 @@
-#include <iostream>
-#include <cunistd>
-
-using namespace std;
-
-#define ever (;;)
-int main(){
-    
-   // for ever;
-    int fpid1, fpid2;
-    fpid1 = fork();
-
-    if (fpid1 < 0) {
-        cout << "创建进程失败" << endl; //创建进程失败
-    } else if(fpid1 == 0) { //返回0是子进程
-        cout << "这是第一个子进程, pid为 " + getpid()) << endl;
-    } else {    //返回2是父进程
-        cout << "这是父进程, pid " + getpid()) << endl;
-
-        fpid2 = fork();
-        if (fpid <0) {
-            cout << "创建第二个进程失败" << endl;
-        } else if (fpid2 == 0) {
-            cout << "这是第二个子进程，pid为 " + getpid()) << endl;
-        } else {
-            cout << "这是第二个子进程的父进程，pid为 " + getpid() << endl;
-        }
-    }
-
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main (void)
+{
+   pid_t pid1,pid2;   //进程标识符
+   pid1 = fork();      //创建一个新的进程
+   if(pid1 < 0)        //如果返回的pid小于0则标示创建进程失败
+     {
+        printf("创建进程失败!\n");
+        exit(1);    //fork出错，退出
+     }
+   else if(pid1 == 0)  //如果pid为0则表示当前执行的是子进程
+   {
+     printf("这是子进程1,进程标识符是%d\n",getpid());
+   }
+   else               //否则为夫进程
+   {
+     printf("这是父进程，进程标识符是%d\n",getpid());
+     pid2 = fork();
+     if(pid2 < 0)
+     {
+        printf("创建第二个进程失败！\n");
+        exit(2);
+     }
+     else if(pid2 == 0) //第二个子进程
+     {
+        printf("这是子进程2，进程标识符是%d\n",getpid()); 
+     }
+     else
+     {
+        printf("这是子进程2的父进程，进程标志是%d\n",getpid());
+     }
+   }
+   printf("这是一个多进程测试，即将退出!\n");
+   return 0;      //返回
 }
+
