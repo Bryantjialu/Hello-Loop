f46afd425b7015c16f11797ed60b402a41243a00
bryantjialu <847124185@qq.com>
2018-03-21 12:21:51 +0800

    fork

--- helloLoop.cpp
+++ helloLoop.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
-#include <stdlib.h>
+#include <cunistd>
 
 using namespace std;
 
 #define ever (;;)
 int main(){
     
-    for ever;
+   // for ever;
+    int fpid;
+    fpid = fork();
 
+    if (fpid < 0) {
+        cout << "error in fork!" << endl;
+    } else if(fpid == 0) {
+        cout << "this is child process, pid " + getpid());
+    } else {
+        cout << "this is parent process, pid " + getpid());
+    }
 
-    system("pause");
     return 0;
 }
