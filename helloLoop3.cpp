55aad2f45e19c6100efca845943e8062fb08f35b
bryantjialu <847124185@qq.com>
2018-03-21 18:48:14 +0800

    1

---
+++ helloLoop3.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define ever (;;)
+
+void createChild(int n, int flag)
+{
+    if (n == 4)
+    {
+        return;
+    }
+
+    if (flag < 0)
+    { //如果返回的pid小于0则标示创建进程失败
+        printf("创建第%d个进程失败！\n", n);
+    }
+    else if (flag == 0)
+    { //如果pid为0则表示当前执行的是子进程
+        printf("这是子进程%d，进程标识符是%d\n", n, getpid());
+        pid_t pid = fork();
+        createChild(n+1,pid);
+    }
+    else
+    { //否则为父进程
+        printf("这是子进程%d的父进程，进程标志是%d\n", n, getpid());
+        pid_t pid = fork();
+        createChild(n+1,pid);
+    }
+}
+int main(void)
+{
+    pid_t pid1; //进程标识符
+
+    pid1 = fork(); //创建一个新的进程
+    createChild(1, pid1);
+
+    for ever;
+
+    return 0; //返回
+}
