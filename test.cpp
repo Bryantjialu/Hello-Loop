d3138deb3b5ae493b002cce6b827d0e354970383
bryantjialu <847124185@qq.com>
2018-03-26 17:14:23 +0800

    5

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <time.h>
+#include <unistd.h>
+using namespace std;
+
+int main() {
+    while (true) {
+        clock_t start, end;
+        start = clock();
+
+        while (clock() - start < 5 * 1000000) {
+
+        }
+        cout << "sleeping" << endl;
+        sleep(5); //linux下是5swindows下是5微秒
+    }
+}
